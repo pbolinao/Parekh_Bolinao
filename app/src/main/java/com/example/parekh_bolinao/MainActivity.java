5511f84c82bec063ee571fd1d0c6420622ec156b
dhruvparekh01 <dhruvparekh01@gmail.com>
2020-02-26 17:18:05 -0800

    Firebase test

--- app/src/main/java/com/example/parekh_bolinao/MainActivity.java
+++ app/src/main/java/com/example/parekh_bolinao/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.parekh_bolinao;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,7 +19,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +33,35 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        User user = new User("afas", "sfsafd@fds.asf");
+
+        Log.d("---------------", "-----------------");
+
+        Task setValueTask = mDatabase.child("users").child("34eqwsda").setValue(user);
+
+        setValueTask.addOnSuccessListener(new OnSuccessListener() {
+            @Override
+            public void onSuccess(Object o) {
+                Toast.makeText(MainActivity.this,"Student added.",Toast.LENGTH_LONG).show();
+                Log.d("---------------", "s-----------------");
+            }
+        });
+
+        setValueTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this,
+                        "something went wrong.\n" + e.toString(),
+                        Toast.LENGTH_SHORT).show();
+                Log.d("---------------", "f-----------------");
+            }
+        });
+
+        Log.d("---------------", "End-----------------");
+
     }
 
 }
